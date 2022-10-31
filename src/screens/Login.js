d53a3a890de28501a0760ff08bc2cf8d0f9ce5dd
kdbswo <kdbswo@gmail.com>
2022-10-31 12:55:13 +0900

    login storage

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,11 +1,12 @@
+import { gql, useMutation } from "@apollo/client";
 import {
   faFacebookSquare,
   faInstagram,
 } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useState } from "react";
 import { useForm } from "react-hook-form";
 import styled from "styled-components";
+import { logUserIn } from "../apollo";
 import BottomBox from "../components/auth/BottmBox";
 import Button from "../components/auth/Button";
 import AuthLayout from "../components/auth/Container";
@@ -24,12 +25,55 @@ const FacebookLogin = styled.div`
   }
 `;
 
+const LOGIN_MUTATION = gql`
+  mutation login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      ok
+      token
+      error
+    }
+  }
+`;
+
 function Login() {
-  const { register, handleSubmit, errors, formState } = useForm({
+  const {
+    register,
+    handleSubmit,
+    errors,
+    formState,
+    getValues,
+    setError,
+    clearErrors,
+  } = useForm({
     mode: "onChange",
   });
+  const onCompleted = (data) => {
+    const {
+      login: { ok, error, token },
+    } = data;
+    if (!ok) {
+      return setError("result", {
+        message: error,
+      });
+    }
+    if (token) {
+      logUserIn(token)
+    }
+  };
+  const [login, { loading }] = useMutation(LOGIN_MUTATION, {
+    onCompleted,
+  });
   const onSubmitValid = (data) => {
-    // console.log(data);
+    if (loading) {
+      return;
+    }
+    const { username, password } = getValues();
+    login({
+      variables: { username, password },
+    });
+  };
+  const clearLoginError = () => {
+    clearErrors("result");
   };
   return (
     <AuthLayout>
@@ -48,6 +92,7 @@ function Login() {
               },
             })}
             name="username"
+            onChange={clearLoginError}
             type="text"
             placeholder="사용자 이름"
             hasError={Boolean(errors?.username?.message)}
@@ -55,14 +100,20 @@ function Login() {
           <FormError message={errors?.username?.message} />
           <Input
             ref={register({ required: "Password is required" })}
+            onChange={clearLoginError}
             name="password"
             type="password"
             placeholder="비밀번호"
             hasError={Boolean(errors?.password?.message)}
           />
           <FormError message={errors?.password?.message} />
-          <Button type="submit" value="로그인" disabled={!formState.isValid} />
+          <Button
+            type="submit"
+            value={loading ? "loading..." : "로그인"}
+            disabled={!formState.isValid || loading}
+          />
         </form>
+        <FormError message={errors?.result?.message} />
         <Separator />
         <FacebookLogin>
           <FontAwesomeIcon icon={faFacebookSquare} />
