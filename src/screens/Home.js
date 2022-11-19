b07b2cbcbe34c1da4ce0de1fb4c315294a838ad0
kdbswo <kdbswo@gmail.com>
2022-11-20 08:06:07 +0900

    fragment isLiked

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,19 +1,9 @@
 import { gql, useQuery } from "@apollo/client";
-import {
-  faBookmark,
-  faComment,
-  faHeart,
-  faPaperPlane,
-} from "@fortawesome/free-regular-svg-icons";
-import { faHeart as SolidHeart } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useHistory } from "react-router-dom";
-import styled from "styled-components";
-import { logUserOut } from "../apollo";
-import Avatar from "../components/Avatar";
-import { FatText } from "../components/shared";
+import Photo from "../components/feed/Photo";
+import PageTitle from "../components/PageTitle";
 
-const FEED_QUERY = gql`
+export const FEED_QUERY = gql`
   query seeFeed {
     seeFeed {
       id
@@ -32,91 +22,15 @@ const FEED_QUERY = gql`
   }
 `;
 
-const PhotoContainer = styled.div`
-  background-color: white;
-  border: 1px solid ${(props) => props.theme.borderColor};
-  margin-bottom: 20px;
-  max-width: 615px;
-  border-radius: 7px;
-`;
-const PhotoHeader = styled.div`
-  padding: 15px;
-  display: flex;
-  align-items: center;
-`;
-
-const Username = styled(FatText)`
-  margin-left: 5px;
-`;
-
-const PhotoFile = styled.img`
-  width: 100%;
-`;
-
-const PhotoData = styled.div`
-  padding: 15px;
-`;
-
-const PhotoActions = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  div {
-    display: flex;
-    align-items: center;
-  }
-  svg {
-    font-size: 20px;
-  }
-`;
-
-const PhotoAction = styled.div`
-  margin-right: 10px;
-`;
-
-const Likes = styled(FatText)`
-  margin-top: 15px;
-  display: block;
-`;
-
 function Home() {
   const { data } = useQuery(FEED_QUERY);
   const history = useHistory();
   console.log(data);
   return (
     <div>
+      <PageTitle title="Home" />
       {data?.seeFeed?.map((photo) => (
-        <PhotoContainer key={photo.id}>
-          <PhotoHeader>
-            <Avatar lg url={photo.user.avatar} />
-            <Username>{photo.user.username}</Username>
-          </PhotoHeader>
-          <PhotoFile src={photo.file} />
-          <PhotoData>
-            <PhotoActions>
-              <div>
-                <PhotoAction>
-                  <FontAwesomeIcon
-                    style={{ color: photo.isLiked ? "tomato" : "inherit" }}
-                    icon={photo.isLiked ? SolidHeart : faHeart}
-                  />
-                </PhotoAction>
-                <PhotoAction>
-                  <FontAwesomeIcon icon={faComment} />
-                </PhotoAction>
-                <PhotoAction>
-                  <FontAwesomeIcon icon={faPaperPlane} />
-                </PhotoAction>
-              </div>
-              <div>
-                <FontAwesomeIcon icon={faBookmark} />
-              </div>
-            </PhotoActions>
-            <Likes>
-              {photo.likes === 1 ? "1 like" : `${photo.likes} likes`}
-            </Likes>
-          </PhotoData>
-        </PhotoContainer>
+        <Photo key={photo.id} {...photo} />
       ))}
     </div>
   );
