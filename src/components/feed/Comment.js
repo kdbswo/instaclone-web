aa5fd0c76d3965e69dc6abe9c46a8b0a88429f4a
kdbswo <kdbswo@gmail.com>
2022-11-27 12:13:34 +0900

    linked Hashtags in Comment

--- src/components/feed/Comment.js
+++ src/components/feed/Comment.js
@@ -1,12 +1,14 @@
+import React from "react";
 import sanitizeHtml from "sanitize-html";
 import styled from "styled-components";
 import { FatText } from "../shared";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 const CommentContainer = styled.div``;
 const CommentCaption = styled.span`
   margin-left: 10px;
-  mark {
+  a {
     background-color: inherit;
     color: ${(props) => props.theme.accent};
     cursor: pointer;
@@ -17,20 +19,20 @@ const CommentCaption = styled.span`
 `;
 
 function Comment({ author, payload }) {
-  const cleandPayload = sanitizeHtml(
-    payload?.replace(/#[\w]+/g, "<mark>$&</mark>"),
-    {
-      allowedTags: ["mark"],
-    }
-  );
   return (
     <CommentContainer>
       <FatText>{author}</FatText>
-      <CommentCaption
-        dangerouslySetInnerHTML={{
-          __html: cleandPayload,
-        }}
-      ></CommentCaption>
+      <CommentCaption>
+        {payload?.split(" ").map((word, index) =>
+          /#[\w]+/.test(word) ? (
+            <React.Fragment key={index}>
+              <Link to={`/hashtags/${word}`}>{word}</Link>{" "}
+            </React.Fragment>
+          ) : (
+            <React.Fragment key={index}>{word} </React.Fragment>
+          )
+        )}
+      </CommentCaption>
     </CommentContainer>
   );
 }
